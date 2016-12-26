7026f975d6cfeb1aeee5289be3eb784f626e52f0
Alex Timonov <timalex1206@gmail.com>
2016-12-26 22:53:51 +0200

    implement @POST, @PUT and @DELETE

--- src/main/java/ua/timonov/rest/resources/MessageResource.java
+++ src/main/java/ua/timonov/rest/resources/MessageResource.java
@@ -3,27 +3,42 @@ package ua.timonov.rest.resources;
 import ua.timonov.rest.model.Message;
 import ua.timonov.rest.service.MessageService;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
 
 @Path("/messages")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class MessageResource {
 
     private MessageService messageService = new MessageService();
 
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
     public List<Message> getMessages() {
         return messageService.getAllMessages();
     }
 
+    @POST
+    public Message addMessage(Message message) {
+        return messageService.addMessage(message);
+    }
+
+    @PUT
+    @Path("/{messageId}")
+    public Message updateMessage(@PathParam("messageId") long id, Message message) {
+        message.setId(id);
+        return messageService.updateMessage(message);
+    }
+
+    @DELETE
+    @Path("/{messageId}")
+    public void deleteMessage(@PathParam("messageId") long id) {
+        messageService.removeMessage(id);
+    }
+
     @GET
     @Path("/{messageId}")
-    @Produces(MediaType.APPLICATION_JSON)
     public Message getMessage(@PathParam("messageId") long messageId) {
         return messageService.getMessage(messageId);
     }
